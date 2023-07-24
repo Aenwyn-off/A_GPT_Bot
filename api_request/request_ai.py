f8782c52ac37688c812b40be3742c6864cea30ec
PepegaHappy <RXSunr1se@yahoo.com>
2023-07-24 16:39:59 +0300

    first commit

---
+++ api_request/request_ai.py
@@ -0,0 +1,19 @@
+import openai
+
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_response(message):
+    openai.api_key = getenv('OPENAI_KEY')
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.8,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+    )
+    return response
